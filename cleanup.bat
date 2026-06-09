f8d2125e1253b251810f2c3fc2fea5f7b1c63590
Jadson Alves <jadson@email.com>
2026-06-09 17:54:10 -0300

    chore: limpeza de arquivos temporarios

---
+++ cleanup.bat
@@ -0,0 +1,3 @@
+git add -A
+git commit -m "chore: remover arquivo temporario"
+git push origin main
--- push.bat
+++
@@ -1,4 +0,0 @@
-cd /d C:\Users\jadso\OneDrive\Área de Trabalho\Teste-EngSotware
-git add -A
-git commit -m "feat: adaptar testes para API LegendaViva (repositorio externo)"
-git push origin main
